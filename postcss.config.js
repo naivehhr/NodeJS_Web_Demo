7851a3899f2c6916f9d7b362b2c76067afc1fe54
naivehhr <1903283729@qq.com>
2017-03-01 18:23:49 +0800

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+/**
+ * postcss=loader 的配置
+ * 这个东东有点强大
+ */
+module.exports = {
+  plugins: [
+    require('autoprefixer')({ /* ...options */ })
+  ]
+}
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+var HOME_DIR = path.resolve(__dirname, 'dist');
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'js/[name].js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new htmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'index.html',
+			inject: true
+		}),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot-loader', 'babel-loader'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!postcss-loader!sass-loader'
+      }
+    ]
+  }
+};
