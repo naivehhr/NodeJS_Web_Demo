a4529407dceb650e0dbe6967ee34cfcae427c7b1
naivehhr <1903283729@qq.com>
2017-03-01 23:57:29 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+// let store = createStore(todoApp)
+let store
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
